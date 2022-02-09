ccfcd143636f6fdcb192cd158e110a8900d8a9b1
matias <matias@axon-ti.cl>
2022-02-09 19:02:21 +0000

    [ADD] Modulo básico

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'Summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manege Training:
+        - COurses
+        -Sessions
+        - Attendees
+        """,
+    
+    'author': 'Odoo',
+    
+    'website': 'wwww.axonsoftware.cl',
+    
+    'category': 'Training',
+    
+    'version': "0.1",
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
---
+++ odoo_academy/models/curso.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class Curso(models.Model):
+    _name = 'academy.curso'
+    _description = 'Información del curso'
+    
