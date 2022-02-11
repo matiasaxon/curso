069b40851427df3b6338f416147e96be946c21cd
Matías Jorquera <matias@axon-ti.cl>
2022-02-11 13:44:08 +0000

    [ADD] Vistas

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -28,6 +28,8 @@
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        
     ],
     
     'demo': [
