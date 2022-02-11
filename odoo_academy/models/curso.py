0d0d9748d660e97edcb2533f61d5b9c81ce94996
Matías Jorquera <matias@axon-ti.cl>
2022-02-11 16:15:07 +0000

    [ADD] ORM Avanzado

--- odoo_academy/models/curso.py
+++ odoo_academy/models/curso.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Curso(models.Model):
     _name = 'academy.curso'
@@ -16,3 +17,22 @@ class Curso(models.Model):
                              copy=False)
     
     active = fields.Boolean(string='Active', default=True)
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    
+    additional_fee = fields.Float(string='Additional Fee', default=10.00)
+    
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UseError('Base Price cannot bet set as Negative.')
+            
+        self.total_price = self.base_price + self.additional_fee
+        
+        @api.constrains('additional_fee')
+        def _check_additional_fee(self):
+            for record in self:
+                if record.additional_fee < 10.00:
+                    raise ValidationError('Addiotional Fees cannot be less than 10.00: %s' % record.additional_fee)
